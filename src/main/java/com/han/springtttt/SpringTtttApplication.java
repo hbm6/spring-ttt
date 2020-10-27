7b851b38e8d3b5ffeab32b43b04dbe431c03b95f
bing <210881407@qq.com>
2020-10-27 20:17:47 +0800

    ssdfs

---
+++ src/main/java/com/han/springtttt/SpringTtttApplication.java
@@ -0,0 +1,13 @@
+package com.han.springtttt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTtttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTtttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/han/springtttt/SpringTtttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.han.springtttt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTtttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
